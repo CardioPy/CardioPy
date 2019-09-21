f050af77797042c477db880c4b2af713d0ef67ac
Jackie Gottshall <jackie.gottshall@gmail.com>
2019-09-21 16:44:08 -0400

    add packaging file structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cardiopy
+
+This is a lightweight and flexibile package for the analysis of single-lead EKG data.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = 'cardiopy',
+	version = "0.0.1",
+	author = "Jackie Gottshall",
+	author_email = "jackie.gottshall@gmail.com",
+	description = "Analysis package for single-lead clinical EKG data",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url="https://github.com/jag2037/cardiopy",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Indepedent",
+		],
+	python_requires = '>=3.6',
+	)
