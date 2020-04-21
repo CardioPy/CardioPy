131fea2b01d1c0e6600e78171187b57f70ff1120
Tasha <nrecoder@wustl.edu>
2020-04-21 09:48:19 -0400

    test. ignore

--- cardiopy/ekg.py
+++ cardiopy/ekg.py
@@ -1,4 +1,4 @@
-""" This file contains the EKG class 
+""" This file contains the EKG class . Testing
 
     All R peak detections should be manually inspected with EKG.plotpeaks method and
     false detections manually removed with rm_peaks method. After rpeak examination, 
