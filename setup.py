304cb733578f5593ea73e04f39e40e79d3cf1111
Jackie Gottshall <jackie.gottshall@gmail.com>
2021-09-21 19:33:17 -0400

    Update version to 1.0,0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name = 'cardiopy',
-	version = "0.0.5",
+	version = "1.0.0",
 	author = "Jackie Gottshall",
 	author_email = "jackie.gottshall@gmail.com",
 	description = "Analysis package for single-lead clinical EKG data",
